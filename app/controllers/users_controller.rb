407ff28da7b9303877b0c969b3c3300ebfe16d44
ya-sho <78021007+ya-sho@users.noreply.github.com>
2021-02-02 14:50:03 +0900

    もれ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,11 @@ class UsersController < ApplicationController
 
   def update
     if current_user.update(user_params)
-    redirect_to root_path
-    else   
+      redirect_to root_path
+    else
       render :edit
     end
     
-
   end
 
   private
